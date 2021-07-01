6db6e9d1dd86ebcd874fffcfb81197b1b910cfc6
coderskm <sumitkumarmishra.code@gmail.com>
2021-07-01 13:08:57 +0530

    committed on divisionbranch

---
+++ division.py
@@ -0,0 +1,6 @@
+print("Hello World ! Welcome to Python calculator")
+num1 = int(input("Enter dividend:"))
+num2 = int(input("Enter divisor:"))
+q = num1/num2
+print("quotient of both the numbers is: "+str(q))
+print("This is Great !!!")
