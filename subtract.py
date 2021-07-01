e134476df98ae66f21ceda4283c91481884f48fd
coderskm <sumitkumarmishra.code@gmail.com>
2021-07-01 12:54:34 +0530

    committed files add.py and subtract.py

--- subtract.py
+++ subtract.py
@@ -1,6 +1,6 @@
 print("Hello World ! Welcome to Python calculator")
 num1 = int(input("Enter number 1:"))
 num2 = int(input("Enter number 2:"))
-sum = num1 + num2
-print("sum of both the numbers is: "+str(sum))
+diff = num1 - num2
+print("sum of both the numbers is: "+str(diff))
 print("This is Great !!!")
